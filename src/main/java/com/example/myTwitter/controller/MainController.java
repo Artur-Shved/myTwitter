2472fcb03674bacbe779d0f6d44bf626e477ded9
Artur Shved <Artur.Shved0202@gmail.com>
2021-01-10 19:35:54 +0200

    ini4

--- src/main/java/com/example/myTwitter/controller/MainController.java
+++ src/main/java/com/example/myTwitter/controller/MainController.java
@@ -4,15 +4,20 @@ import com.example.myTwitter.domain.Message;
 import com.example.myTwitter.domain.User;
 import com.example.myTwitter.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
@@ -20,24 +25,45 @@ public class MainController {
     @Autowired
     MessageRepo messageRepo;
 
+    @Value("${upload.path}")
+    private String uploadPath;
+
     @GetMapping("/")
     public String greeting(Map<String, Object> model){
         return "greeting";
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model){
-        Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
+        Iterable<Message>messages = messageRepo.findAll();
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }else{
+            messages = messageRepo.findAll();
+        }
+        model.addAttribute("messages",messages);
+        model.addAttribute("filter",filter);
         return "main";
     }
 
     @PostMapping("/main")
     public String add(@AuthenticationPrincipal User user,
-            @RequestParam String text,
-            @RequestParam String tag,
-            Map<String, Object> model) {
+                      @RequestParam String text,
+                      @RequestParam String tag, Map<String, Object> model,
+                      @RequestParam("file") MultipartFile file) throws IOException {
+
         Message message = new Message(text, tag, user);
+        if(file != null){
+            File uploadDir = new File(uploadPath);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadPath + "/" +  resultFilename));
+            message.setFilename(resultFilename);
+
+        }
 
         messageRepo.save(message);
 
@@ -48,17 +74,6 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("/filter")
-    public String filter(@RequestParam String filter,
-                         Map<String, Object> model){
-        Iterable<Message>messages;
-        if(filter != null && !filter.isEmpty()) {
-             messages = messageRepo.findByTag(filter);
-        }else{
-            messages = messageRepo.findAll();
-        }
-        model.put("messages", messages);
-        return "main";
-    }
+
 
 }
--- src/main/java/com/example/myTwitter/domain/Message.java
+++ src/main/java/com/example/myTwitter/domain/Message.java
@@ -4,36 +4,46 @@ import javax.persistence.*;
 
 @Entity
 public class Message{
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
+
     private String text;
     private String tag;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "usr_id")
     private User author;
 
+    private String filename;
 
-
+    public Message(String text, String tag, User user) {
+        this.author = user;
+        this.text = text;
+        this.tag = tag;
+    }
     public Message() {
     }
 
-    public Message(String text, String tag, User author) {
-        this.text = text;
-        this.tag = tag;
-        this.author = author;
-    }
-
-    public String getAuthorName(){
+    public String getAuthorName() {
         return author != null ? author.getUsername() : "<none>";
     }
 
-    public User getAuthor () {
+    public User getAuthor() {
         return author;
     }
 
     public void setAuthor(User author) {
         this.author = author;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
     }
 
     public Integer getId() {
@@ -44,19 +54,19 @@ public class Message{
         this.id = id;
     }
 
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
     public String getTag() {
         return tag;
     }
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
